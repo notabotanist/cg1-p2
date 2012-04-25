b68640a9d0de61c25505e083092d4200ac721f55
Matt MacEwan(GLaDOS) <mrm4677@rit.edu>
2012-04-25 15:37:34 -0400

    Front face of cube tessellation

--- renderings.cpp
+++ renderings.cpp
@@ -2,12 +2,12 @@
 //
 // File:  renderings.cpp
 // Authors:  G. Fotiades, A. Koutmos
-// Contributors: <your name here>
+// Contributors: Matthew MacEwan
 // Last modified: 2/2/11
 //
 // Description:  This file holds the implementations cube/sphere/cylinder/cone
-//               functions.  These will be linked to framework at link time,
-//               when the final binary is produced.
+//			   functions.  These will be linked to framework at link time,
+//			   when the final binary is produced.
 //
 ////////////////////////////////////////////////////////////
 
@@ -17,24 +17,38 @@
 extern void addTriangle(Point3 p1, Point3 p2, Point3 p3);
 
 // Window title
-const char* PROJECT_NAME = "Project 2 - Tessellation (<first_name> <last_name>)";
+const char* PROJECT_NAME = "Project 2 - Tessellation (Matthew MacEwan)";
 
 void Cube(int n){
-    // Your Cube code goes here
-    return;
+	// Your Cube code goes here
+	float step = 1.0 / n;
+	// iterate over rows
+	for (int i = 0; i < n; i++) {
+		// iterate over columns
+		for (int j = 0; j < n; j++) {
+			// front face
+			addTriangle(Point3(-0.5 + j * step, 0.5 - i * step, 0.5),
+						Point3(-0.5 + j * step, 0.5 - (i+1) * step, 0.5),
+						Point3(-0.5 + (j+1) * step, 0.5 - i * step, 0.5));
+			addTriangle(Point3(-0.5 + (j+1) * step, 0.5 - i * step, 0.5),
+						Point3(-0.5 + j * step, 0.5 - (i+1) * step, 0.5),
+						Point3(-0.5 + (j+1) * step, 0.5 - (i+1) * step, 0.5));
+		}
+	}
+	return;
 }
 
 void Cone(int n, int m){
-    // Your Cone code goes here
-    return;
+	// Your Cone code goes here
+	return;
 }
 
 void Cylinder(int n, int m){
-    // Your Cylinder code goes here
-    return;
+	// Your Cylinder code goes here
+	return;
 }
 
 void Sphere(int n){
-    // Your Sphere code goes here
-    return;
+	// Your Sphere code goes here
+	return;
 }
